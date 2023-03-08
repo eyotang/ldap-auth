f4a992d7681007b2aabcf8e43e82aec75cce184a
tangyongqiang@hypergryph.com <tangyongqiang@hypergryph.com>
2023-03-08 17:00:53 +0800

    feat: 增加用例覆盖

--- search_test.go
+++ search_test.go
@@ -13,6 +13,11 @@ func TestSearch_Users(t *testing.T) {
 
 		user, err := client.SearchUser("tangyongqiang" + client.cfg.EmailSuffix)
 		So(err, ShouldBeNil)
+		So(user, ShouldNotBeNil)
 		So(user.Username, ShouldEqual, "tangyongqiang")
+
+		user2, err := client.SearchUser("tangyongqiang2" + client.cfg.EmailSuffix)
+		So(err, ShouldBeNil)
+		So(user2, ShouldBeNil)
 	})
 }
